4b9f985960a580311a006ec7b26a68cfd69bdded
Kumar Kartikay <krkartikay@gmail.com>
2018-03-12 23:28:28 +0530

    Form frontend working now

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -10,6 +10,11 @@ def main_page():
 def submit_link():
     return render_template("submit.html")
 
+@app.route('/add_link')
+def add_link():
+    # do stuff
+    return "Link submitted successfully"
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
