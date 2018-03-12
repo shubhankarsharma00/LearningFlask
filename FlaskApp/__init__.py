996b91b0e30ff34ac8f0100167f0d943e0074ce2
shubhankarsharma78@gmail.com <shubhankarsharma78@gmail.com>
2018-03-12 22:37:03 +0530

    new link added

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/newlink')
+def addnewlink():
+    return render_template("newlink.html")
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
