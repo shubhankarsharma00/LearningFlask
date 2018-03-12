ca385bab57c5195e1eebbf11b03375371def3c4e
Kumar Kartikay <krkartikay@gmail.com>
2018-03-12 23:20:11 +0530

    Rename 'newlink' to submit

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -3,12 +3,12 @@ from flask import *
 app = Flask(__name__)	
 
 @app.route('/')
-def hello_world():
+def main_page():
     return render_template("index.html")
 
-@app.route('/newlink')
-def addnewlink():
-    return render_template("newlink.html")
+@app.route('/submit_link')
+def submit_link():
+    return render_template("submit.html")
 
 if __name__ == '__main__':
     app.debug=True
