7eabef774c370fc3c44cf10373537d7ce9994b78
Kumar Kartikay <krkartikay@gmail.com>
2018-03-13 18:46:49 +0530

    Change to POST

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,19 +1,20 @@
 from flask import *
 
-app = Flask(__name__)	
+app = Flask(__name__)
+app.secret_key = 'T\xa0\x96W\x1cZ\x02\x81zZ\xf0\xbd\xe1"+\x05\x83\x1fc\xde]y>\xe3'
 
 @app.route('/')
 def main_page():
     return render_template("index.html")
 
-@app.route('/submit_link')
+@app.route('/submit_link', methods=['POST','GET'])
 def submit_link():
-    return render_template("submit.html")
-
-@app.route('/add_link')
-def add_link():
-    # do stuff
-    return "Link submitted successfully"
+    if request.method == 'POST':
+        # database stuff
+        flash("Link Submitted!")
+        return redirect(url_for("main_page"))
+    else:
+        return render_template("submit.html")
 
 if __name__ == '__main__':
     app.debug=True
